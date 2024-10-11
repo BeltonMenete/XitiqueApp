bd1cd3aa751c51189094a676d49dc3d6a1537a3a
DevAlberto <beltonmenete@hotmail.com>
2024-10-11 18:26:17 +0200

    Rebuild the project

--- Models/DailyDeposit.cs
+++
@@ -1,9 +0,0 @@
-namespace XitiqueAPI.Models
-{
-    public class DailyDeposit
-    {
-        public byte[] Id { get; set; } = Ulid.NewUlid().ToByteArray();
-        public decimal DailyAccumulated { get; set; }
-    }
-}
-
--- Models/TotalDailyDeposits.cs
+++
@@ -1,8 +0,0 @@
-namespace XitiqueAPI.Models
-{
-    public class TotalDailyDeposits
-    {
-        public byte[] Id { get; set; } = Ulid.NewUlid().ToByteArray();
-        public decimal Amount { get; set; }
-    }
-}
