305fbb517d652f79b1b386d501117135638e01e6
DevAlberto <beltonmenete@hotmail.com>
2024-10-11 12:20:54 +0200

    AdddbMigrations

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -6,4 +6,9 @@ namespace XitiqueAPI.Data;
 public class AppDbContext(DbContextOptions options) : DbContext(options)
 {
     public DbSet<Client> Clients { get; set; }
+    public DbSet<DailyDeposit> DailyDeposits { get; set; }
+    public DbSet<Loan> Loans { get; set; }
+    public DbSet<TotalAllClientsBalance> TotalAllClientsBalance { get; set; }
+    public DbSet<TotalDailyDeposits> TotalDailyDeposits { get; set; }
+    public DbSet<Transaction> Transactions { get; set; }
 }
